8294090f46769ab4bf8913780606a90ecb03d2b5
R4posaG <henriqueraposo05@gmail.com>
2026-07-20 00:03:44 +0100

    src

---
+++ src/stack_utils.c
@@ -0,0 +1,50 @@
+#include "push_swap.h"
+
+t_stack	*create_node(int value)
+{
+	t_stack	*new_node;
+
+	new_node = malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->index = -1; // Inicializado a -1 até ser calculado
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void	stack_add_back(t_stack **stack, t_stack *new_node)
+{
+	t_stack *current;
+
+	if (!stack || !new_node)
+		return ;
+	if (!*stack)
+	{
+		*stack = new_node;
+		return ;
+	}
+	current = *stack;
+	while (current->next)
+		current = current->next;
+	current->next = new_node;
+	new_node->prev = current;
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*next_node;
+	t_stack	*current;
+
+	if (!stack || !*stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+	*stack = NULL;
+}
